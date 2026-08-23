ec25ae2903cf1bea4abddbc6e2b5479e89cb8899
massimiliano regis <massimiliano.regis@gmail.com>
2026-08-23 14:31:15 +0300

    Bump Android gateway to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.simpleservice.smsgateway"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
     }
 
     compileOptions {
